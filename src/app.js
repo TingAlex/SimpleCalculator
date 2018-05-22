bfd17f5497e1301496f7116650d1db9ccd88994f
Ting <tingalex30@gmail.com>
2018-05-22 16:31:05 +0800

    boilplate config

---
+++ src/app.js
@@ -0,0 +1,8 @@
+// import validator from "validator";
+// console.log(validator.isEmail("ting@email.com"));
+import React from "react";
+import ReactDOM from "react-dom";
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
+
+ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById("app"));
